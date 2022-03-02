c22a8cbe44b5af8b13ddb7c74cc97e2d9f9706b8
tian <tianwu@outlook.com>
2022-03-02 12:25:16 +1100

    add rs script for medm interface

---
+++ softioc/rshake_ctrl/python/rs_service.py
@@ -0,0 +1,31 @@
+from pcaspy import Driver, SimpleServer
+import rsconnect
+
+rsPrefix = "RS_"
+
+rsDB = {
+    'COUNT': {'type': 'int'},
+}
+
+
+class MyDriver(Driver):
+    def __init__(self):
+        super().__init__()
+        self.rs = rsconnect.RSConnect(host="", port=9988, interval=0.125)
+        self.rs.start()
+
+    def read(self, reason):
+        if reason == 'COUNT':
+            value = self.rs.get_count()
+            self.setParam('COUNT', value)
+            return value
+
+
+if __name__ == '__main__':
+    server = SimpleServer()
+    server.createPV(rsPrefix, rsDB)
+    driver = MyDriver()
+
+    while True:
+        server.process(0.1)
+        driver.updatePVs()
