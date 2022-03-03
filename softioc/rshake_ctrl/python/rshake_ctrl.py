175584d782e2dc172bd14543f23dd7be04d54ab0
tian <tianwu@outlook.com>
2022-03-03 15:02:01 +1100

    add placeholder for rshake ctrl script

---
+++ softioc/rshake_ctrl/python/rshake_ctrl.py
@@ -0,0 +1,24 @@
+import os.path
+import datetime
+import systemd.daemon
+from pcaspy import Driver, SimpleServer
+from time import sleep
+
+# local script
+import rsconnect
+
+# Version
+
+# Epics channel for RBOOM ctrl
+
+# rboom prefix
+
+# generate rboom database
+
+# generate ini file for server to record channel data
+
+# main driver class
+
+# local test and run
+if __name__ == "__main__":
+    pass
