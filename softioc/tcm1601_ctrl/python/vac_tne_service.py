efb3e85a85a46318fd8b3cb8ada75262906264af
tian3rd <tianwu@outlook.com>
2022-05-04 12:45:30 +1000

    add logging, hide errors with try-except

--- softioc/tcm1601_ctrl/python/vac_tne_service.py
+++ softioc/tcm1601_ctrl/python/vac_tne_service.py
@@ -1,10 +1,13 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+from distutils.log import INFO
 import re
 from pcaspy import Driver, SimpleServer
 import Tcm1601
 from time import sleep, time
+import logging
+from datetime import datetime as dt
 
 import systemd.daemon
 import datetime
@@ -27,11 +30,14 @@ controllerDB = {
     'ELAPSED_TIME': {'type': 'str'},
 }
 
+logging.basicConfig(filename=dt.today().strftime('%Y-%m-%d') + '_log', filemode='a', format='%(asctime)s - %(message)s', datefmt='%d-%b-%y %H:%M:%S', level=logging.INFO)
+
 
 class myDriver(Driver):
     def __init__(self):
         super().__init__()
         self.controller = Tcm1601.TCM1601('/dev/ttyUSB0')
+        self.script_start = time()
         self.start_timing = -1
         self.elapsed = -2
         self.tid = threading.Thread(target=self.run)
@@ -40,65 +46,85 @@ class myDriver(Driver):
 
     def read_channels(self, reason):
         value = 'NULL'
-        if reason == 'MOTOR_TMP':
-            value = 1 if self.controller.get_turbopump_status() == 'ON' else 0
-        elif reason == 'ELAPSED_TIME':
-            value = '{t} s'.format(t=self.elapsed)
-        elif reason == 'ERROR_CODE':
-            value = self.controller.get_error()
-        elif reason == 'ERROR_LAST':
-            value = self.controller.get_last_error()
-        elif reason == 'ACT_ROT_SPD':
-            value = self.controller.get_act_rotspd()
-            spd = int(value.split()[0])
-            if spd > 1 and spd < 599:
-                self.elapsed = time() - self.start_timing
-            # self.setParam('ELAPSED_TIME', '{t} s'.format(t=self.elapsed))
-            # self.updatePVs()
-        elif reason == 'TMP_I_MOT':
-            value = self.controller.get_motor_current()
-        elif reason == 'TMP_OP_HRS':
-            value = self.controller.get_operation_hours()
-        elif reason == 'PRESSURE':
-            value = self.controller.get_pressure()
-        elif reason == 'ADDRESS':
-            value = self.controller.get_address()
-        elif reason == 'SWITCH_PNT':
-            value = self.controller.get_switch_point()
-        elif reason == 'TMS_ACT_TMP':
-            value = self.controller.get_tms_act_tmp()
+        try: 
+            if reason == 'MOTOR_TMP':
+                value = 1 if self.controller.get_turbopump_status() == 'ON' else 0
+            elif reason == 'ELAPSED_TIME':
+                value = '{t} s'.format(t=self.elapsed)
+            elif reason == 'ERROR_CODE':
+                value = self.controller.get_error()
+            elif reason == 'ERROR_LAST':
+                value = self.controller.get_last_error()
+            elif reason == 'ACT_ROT_SPD':
+                value = self.controller.get_act_rotspd()
+                spd = int(value.split()[0])
+                if spd > 1 and spd < 599:
+                    self.elapsed = time() - self.start_timing
+                # self.setParam('ELAPSED_TIME', '{t} s'.format(t=self.elapsed))
+                # self.updatePVs()
+            elif reason == 'TMP_I_MOT':
+                value = self.controller.get_motor_current()
+            elif reason == 'TMP_OP_HRS':
+                value = self.controller.get_operation_hours()
+            elif reason == 'PRESSURE':
+                value = self.controller.get_pressure()
+            elif reason == 'ADDRESS':
+                value = self.controller.get_address()
+            elif reason == 'SWITCH_PNT':
+                value = self.controller.get_switch_point()
+            elif reason == 'TMS_ACT_TMP':
+                value = self.controller.get_tms_act_tmp()
+        except Exception as e:
+            err_msg = "ERROR occurred while reading: {}".format(str(e))
+            print(err_msg)
+            logging.error(err_msg)
 
         self.setParam(reason, value)
         self.updatePVs()
         return value
 
     def write(self, reason, value):
-        if reason == 'MOTOR_TMP':
-            self.start_timing = time()
-            success = self.controller.turn_on_turbopump() if int(
-                value) == 1 else self.controller.turn_off_turbopump()
-            if not success:
-                print('Error turning on/off turbopump')
-        elif reason == 'ADDRESS':
-            value = int(value)
-            self.controller.set_address(value)
-        elif reason == 'SWTICH_PNT':
-            value = int(value)
-            success = self.controller.set_switch_point(value)
-            if not success:
-                print('Failed to set switch point')
+        try: 
+            if reason == 'MOTOR_TMP':
+                self.start_timing = time()
+                success = self.controller.turn_on_turbopump() if int(
+                    value) == 1 else self.controller.turn_off_turbopump()
+                if not success:
+                    print('Error turning on/off turbopump')
+            elif reason == 'ADDRESS':
+                value = int(value)
+                self.controller.set_address(value)
+            elif reason == 'SWTICH_PNT':
+                value = int(value)
+                success = self.controller.set_switch_point(value)
+                if not success:
+                    print('Failed to set switch point')
+        except Exception as e:
+            err_msg = "ERROR occurred while writing: {}".format(str(e))
+            print(err_msg)
+            logging.error(err_msg)
         self.setParam(reason, value)
         self.read(reason)
         self.updatePVs()
 
     def run(self):
         while True:
-            for reason in controllerDB.keys():
-                # if reason != 'ELAPSED_TIME':
-                print("READING: ", reason)
-                self.read_channels(reason)
-                sleep(.1)
-            self.updatePVs()
+            try:
+                current_status = []
+                for reason in controllerDB.keys():
+                    # if reason != 'ELAPSED_TIME':
+                    print("READING: ", reason)
+                    current_status.append(reason + ": " + str(self.read_channels(reason)))
+                    sleep(.1)
+                self.updatePVs()
+                if time() - self.script_start > 2:
+                    logging.info(f'{" | ".join(current_status)}')
+                    self.script_start = time()
+            except Exception as e:
+                err_msg = "ERROR occurred while running the script: {}".format(str(e))
+                print(err_msg)
+                logging.error(err_msg)
+                continue
 
 
 if __name__ == '__main__':
